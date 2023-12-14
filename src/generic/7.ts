66ebd7fb27b068c98c16e963c71ac1ed5aaff653
Dmytro Sayko <saykodmytro@icloud.com>
2023-12-15 00:00:04 +0200

    finish

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -4,16 +4,18 @@
 */
 
 export enum UserRole {
-  admin = 'admin',
-  editor = 'editor',
-  guest = 'guest',
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
 }
 
+type UserRolesStatuses = Record<UserRole, string>;
+
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+const RoleDescription: UserRolesStatuses = {
+  admin: "Admin User",
+  editor: "Editor User",
+  guest: "Guest User",
 };
 
 export {};
